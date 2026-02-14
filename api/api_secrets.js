1b6a6d484b71d11c537922cd934023eaa7fa90b0
Vasuki_01 <v05vasuki@gmail.com>
2026-02-14 12:01:12 +0530

    Add files via upload

--- api/api_secrets.js
+++ api/api_secrets.js
@@ -1,15 +1,12 @@
-export default function handler(req, res) {
-  try {
-    const secret = process.env.MY_SECRET
-
-    res.status(200).json({
-      message: "API working",
-      success: true
-    })
-
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
-    })
-  }
+export default function handler(req, res) { 
+  try { 
+    const secret = process.env
+    res.status(200).json({ 
+      message: "API working", 
+      success: true , 
+      data:secret 
+    }) 
+  } catch (error) { 
+    res.status(500).json({ error: error.message }) 
+  } 
 }
