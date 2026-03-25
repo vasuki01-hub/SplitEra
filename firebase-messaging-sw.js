e747fb103473eb420f957b8bb0c848d4269edc32
Vasuki_01 <v05vasuki@gmail.com>
2026-03-25 12:12:25 +0530

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 // public/firebase-messaging-sw.js
 
-importScripts('https://www.gstatic.com/firebasejs/10.14.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.14.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 // 🔥 Your Firebase Config (already correct)
 const firebaseConfig = {
