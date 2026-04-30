6c40b4235b9ef023e4e388a1d8fcd3ae1c7d1783
Vasuki_01 <v05vasuki@gmail.com>
2026-04-30 18:19:15 +0530

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,10 +12,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 firebase.initializeApp({
     apiKey: "AIzaSyCu3sYRO65Qq0J0hkbfYfXZKre-_89VuQE",
     authDomain: "animai-studio-jtpei.firebaseapp.com",
+    databaseURL: "https://animai-studio-jtpei-default-rtdb.firebaseio.com",
     projectId: "animai-studio-jtpei",
     storageBucket: "animai-studio-jtpei.firebasestorage.app",
     messagingSenderId: "808906988475",
-    appId: "1:808906988475:web:26768298ec96ac0200429e"
+    appId: "1:808906988475:web:26768298ec96ac0200429e",
+    measurementId: "G-FC3SYSXMGT"
 });
 
 const messaging = firebase.messaging();
